e57c16a1f42a1c7e978eef87fbd8d8a6dc43f832
guivictorr <55333929+guivictorr@users.noreply.github.com>
2021-12-07 21:05:28 -0300

    feat(Logo): created logo component with tests and storybook

---
+++ src/components/Logo/test.tsx
@@ -0,0 +1,51 @@
+import { screen } from '@testing-library/react'
+import theme from 'styles/theme'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Logo from '.'
+
+describe('<Logo />', () => {
+  it('should render white label by default', () => {
+    renderWithTheme(<Logo />)
+
+    expect(screen.getByLabelText(/Won games/i).parentElement).toHaveStyle({
+      color: theme.colors.white
+    })
+  })
+
+  it('should render black label when color black is passed', () => {
+    renderWithTheme(<Logo color="black" />)
+
+    expect(screen.getByLabelText(/Won games/i).parentElement).toHaveStyle({
+      color: theme.colors.black
+    })
+  })
+
+  it('should render a normal size logo by default', () => {
+    renderWithTheme(<Logo />)
+
+    expect(screen.getByLabelText(/Won games/i).parentElement).toHaveStyle({
+      width: '11rem',
+      height: '3.3rem'
+    })
+  })
+
+  it('should render a bigger logo when size large is passed', () => {
+    renderWithTheme(<Logo size="large" />)
+
+    expect(screen.getByLabelText(/Won games/i).parentElement).toHaveStyle({
+      width: '20rem',
+      height: '5.9rem'
+    })
+  })
+
+  it('should render a logo without text if hideOnMobile', () => {
+    renderWithTheme(<Logo hideOnMobile />)
+
+    expect(screen.getByLabelText(/Won games/i).parentElement).toHaveStyleRule(
+      'width',
+      '5.8rem',
+      { media: '(max-width: 768px)' }
+    )
+  })
+})

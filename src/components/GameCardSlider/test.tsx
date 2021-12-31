0a38c46a5e27393b187bd8f27f07da6f24dd89b3
guivictorr <55333929+guivictorr@users.noreply.github.com>
2021-12-31 05:26:45 -0300

    feat(GameCardSlider): created GameCardSlider tests

---
+++ src/components/GameCardSlider/test.tsx
@@ -0,0 +1,77 @@
+import 'match-media-mock'
+import { screen } from '@testing-library/react'
+import theme from 'styles/theme'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import GameCardSlider from '.'
+
+const items = [
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x140',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x141',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x142',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x143',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x144',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  }
+]
+
+describe('<GameCardSlider />', () => {
+  it('should render correctly', () => {
+    const { container } = renderWithTheme(<GameCardSlider items={items} />)
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render 4 active items', () => {
+    const { container } = renderWithTheme(<GameCardSlider items={items} />)
+    expect(container.querySelectorAll('.slick-active')).toHaveLength(4)
+  })
+
+  it('should white arrows by default', () => {
+    renderWithTheme(<GameCardSlider items={items} />)
+    expect(screen.getByLabelText(/previous games/i)).toHaveStyle({
+      color: theme.colors.white
+    })
+    expect(screen.getByLabelText(/next games/i)).toHaveStyle({
+      color: theme.colors.white
+    })
+  })
+
+  it('should black arrows if black color is passed', () => {
+    renderWithTheme(<GameCardSlider items={items} color="black" />)
+    expect(screen.getByLabelText(/previous games/i)).toHaveStyle({
+      color: theme.colors.black
+    })
+    expect(screen.getByLabelText(/next games/i)).toHaveStyle({
+      color: theme.colors.black
+    })
+  })
+})

90d354428ae8cf6f8584664d6c4542db196c304f
guivictorr <55333929+guivictorr@users.noreply.github.com>
2022-01-03 04:38:13 -0300

    feat(checkbox): created checkbox component

---
+++ src/components/Checkbox/test.tsx
@@ -0,0 +1,14 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Checkbox from '.'
+
+describe('<Checkbox />', () => {
+  it('should render with label', () => {
+    renderWithTheme(<Checkbox label="checkbox label" labelFor="check" />)
+
+    expect(screen.getByRole('checkbox')).toBeInTheDocument()
+    expect(screen.getByText(/checkbox label/i)).toBeInTheDocument()
+    expect(screen.getByText(/checkbox label/i)).toHaveAttribute('for', 'check')
+  })
+})

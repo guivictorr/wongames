07fb2634ad8d6289fb25c4a770cd89e6f4b58874
guivictorr <55333929+guivictorr@users.noreply.github.com>
2022-01-05 20:46:43 -0300

    feat(formsignup): created form sign up component

---
+++ src/components/FormSignUp/stories.tsx
@@ -0,0 +1,13 @@
+import { Story, Meta } from '@storybook/react'
+import FormSignUp from '.'
+
+export default {
+  title: 'FormSignUp',
+  component: FormSignUp
+} as Meta
+
+export const Default: Story = () => (
+  <div style={{ width: '36rem', margin: '0 auto' }}>
+    <FormSignUp />
+  </div>
+)

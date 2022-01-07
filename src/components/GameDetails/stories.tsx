31171f842b0c16748ac7ef00302bcf9f5a19422c
guivictorr <55333929+guivictorr@users.noreply.github.com>
2022-01-07 20:08:33 -0300

    docs(gamedetails): added argType for platforms

--- src/components/GameDetails/stories.tsx
+++ src/components/GameDetails/stories.tsx
@@ -1,18 +1,29 @@
 import { Story, Meta } from '@storybook/react'
-import GameDetails from '.'
+import GameDetails, { GameDetailsProps } from '.'
 
 export default {
   title: 'Data display/GameDetails',
   component: GameDetails,
+  argTypes: {
+    platforms: {
+      control: {
+        type: 'inline-check',
+        options: ['mac', 'windows', 'linux']
+      }
+    }
+  },
   parameters: {
     backgrounds: {
       default: 'dark'
     }
+  },
+  args: {
+    platforms: ['mac', 'windows', 'linux']
   }
 } as Meta
 
-export const Default: Story = () => (
+export const Default: Story<GameDetailsProps> = (args) => (
   <div style={{ maxWidth: '140rem', margin: 'auto' }}>
-    <GameDetails platforms={['linux', 'windows', 'mac']} />
+    <GameDetails {...args} />
   </div>
 )

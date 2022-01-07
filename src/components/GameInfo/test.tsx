c8056348f7705978e4004704ac062f206809fd9c
guivictorr <55333929+guivictorr@users.noreply.github.com>
2022-01-07 18:43:26 -0300

    feat(gameinfo): created gameinfo component without styles

---
+++ src/components/GameInfo/test.tsx
@@ -0,0 +1,26 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import GameInfo from '.'
+
+const props = {
+  title: 'Borderlands 3',
+  description: 'My game description',
+  price: '200.00'
+}
+
+describe('<GameInfo />', () => {
+  it('should render the heading, description and price', () => {
+    renderWithTheme(<GameInfo {...props} />)
+    expect(
+      screen.getByRole('heading', { name: props.title })
+    ).toBeInTheDocument()
+    expect(screen.getByText(props.description)).toBeInTheDocument()
+    expect(screen.getByText(`$${props.price}`)).toBeInTheDocument()
+  })
+
+  it('should render buttons', () => {
+    renderWithTheme(<GameInfo {...props} />)
+    expect(screen.getAllByRole('button')).toHaveLength(2)
+  })
+})

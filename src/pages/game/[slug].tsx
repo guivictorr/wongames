a1cda5a1e342fdec97dbece0f38704a656042e3e
guivictorr <55333929+guivictorr@users.noreply.github.com>
2022-01-12 20:13:16 -0300

    feat(game): using getStaticProps and getStaticPaths to fetch data

--- src/pages/game/[slug].tsx
+++ src/pages/game/[slug].tsx
@@ -1,7 +1,30 @@
-import Game from 'templates/Game'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import Game, { GameTemplateProps } from 'templates/Game'
 
-function Index() {
-  return <Game />
+function Index(props: GameTemplateProps) {
+  return <Game {...props} />
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [{ params: { slug: 'cyberpunk-2077' } }],
+    fallback: false
+  }
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  return {
+    props: {
+      cover:
+        'https://images.gog-statics.com/5643a7c831df452d29005caeca24c28cdbfaa6fbea5a9556b147ee26d325fa70_bg_crop_1366x655.jpg',
+      gameInfo: {
+        title: 'Cyberpunk 2077',
+        price: '59.00',
+        description:
+          'Cyberpunk 2077 is an open-world, action-adventure story set in Night City, a megalopolis obsessed with power, glamour and body modification. You play as V, a mercenary outlaw going after a one-of-a-kind implant that is the key to immortality'
+      }
+    }
+  }
 }
 
 export default Index

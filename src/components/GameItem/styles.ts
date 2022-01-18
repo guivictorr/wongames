6be7a6104e10191aa16f1a8042a1ab1a6b4ecb31
guivictorr <55333929+guivictorr@users.noreply.github.com>
2022-01-18 19:54:21 -0300

    feat(gameitem): created gameitem component

---
+++ src/components/GameItem/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div``
+
+export const GameContent = styled.div``
+
+export const ImageBox = styled.div``
+
+export const Image = styled.img``
+
+export const Content = styled.div``
+
+export const Title = styled.h3``
+
+export const Price = styled.p``

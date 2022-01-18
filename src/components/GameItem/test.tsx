48c038d0dfa74f0be4a55879647e67091357c903
guivictorr <55333929+guivictorr@users.noreply.github.com>
2022-01-18 20:41:02 -0300

    feat(gameitem): added payment content to gameitem

--- src/components/GameItem/test.tsx
+++ src/components/GameItem/test.tsx
@@ -33,4 +33,22 @@ describe('<GameItem />', () => {
       screen.getByRole('link', { name: `Get ${props.title} here` })
     ).toHaveAttribute('href', downloadLink)
   })
+
+  it('should render payment info', () => {
+    const paymentInfo = {
+      flag: 'mastercard',
+      img: '/img/master-card.png',
+      number: '**** **** **** 1234',
+      purchaseDate: 'Purchase made on 07/20/2020 at 20:32'
+    }
+    renderWithTheme(<GameItem {...props} paymentInfo={paymentInfo} />)
+
+    expect(screen.getByRole('img', { name: paymentInfo.flag })).toHaveAttribute(
+      'src',
+      paymentInfo.img
+    )
+
+    expect(screen.getByText(paymentInfo.number)).toBeInTheDocument()
+    expect(screen.getByText(paymentInfo.purchaseDate)).toBeInTheDocument()
+  })
 })

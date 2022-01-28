bb930ef4755ce552df5d1090669c7d8dedc84c05
guivictorr <55333929+guivictorr@users.noreply.github.com>
2022-01-27 21:18:56 -0300

    fix(cartlist): fixing cartlist isEmpty prop

--- src/components/CartList/index.tsx
+++ src/components/CartList/index.tsx
@@ -12,7 +12,7 @@ export type CartListProps = {
 }
 
 const CartList = ({ items = [], total, hasButton = false }: CartListProps) => (
-  <S.Wrapper isEmpty={!!items.length}>
+  <S.Wrapper isEmpty={!items.length}>
     {items.length ? (
       <>
         {items.map((item) => (

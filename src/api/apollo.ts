43b30ca7ceb418e304b5c587078c3eeed4f4ddf5
guivictorr <55333929+guivictorr@users.noreply.github.com>
2022-02-01 19:47:15 -0300

    feat(api): creating apollo file to help with ssr and cache

---
+++ src/api/apollo.ts
@@ -0,0 +1,34 @@
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  NormalizedCacheObject
+} from '@apollo/client'
+import { useMemo } from 'react'
+let apolloClient: ApolloClient<NormalizedCacheObject>
+
+function createApolloClient() {
+  return new ApolloClient({
+    ssrMode: typeof window === 'undefined',
+    link: new HttpLink({ uri: 'http://localhost:1337/graphql' }),
+    cache: new InMemoryCache()
+  })
+}
+
+export function initializeApollo(initialState = {}) {
+  const apolloClientGlobal = apolloClient ?? createApolloClient()
+
+  if (initialState) {
+    apolloClientGlobal.cache.restore(initialState)
+  }
+
+  if (typeof window === 'undefined') return apolloClientGlobal
+
+  if (!apolloClient) apolloClient = apolloClientGlobal
+
+  return apolloClientGlobal
+}
+
+export function useApollo(initialState = {}) {
+  return useMemo(() => initializeApollo(initialState), [initialState])
+}

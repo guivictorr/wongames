4ffa2f3ee4da6f3bbc48e6c474ccf5615d78a2ba
guivictorr <55333929+guivictorr@users.noreply.github.com>
2022-02-01 22:00:12 -0300

    feat(games): using apollo to get games from graphql api

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -1,17 +1,34 @@
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import Games, { GamesProps } from 'templates/Games'
 
-import gamesMock from 'components/GameCardSlider/mock'
 import filterItemsMock from 'components/ExploreSidebar/mock'
+import { initializeApollo } from 'api/apollo'
+import { QUERY_GAMES } from 'graphql/queries/games'
+import { QueryGames, QueryGamesVariables } from 'graphql/generated/QueryGames'
 
 function GamesPage(props: GamesProps) {
   return <Games {...props} />
 }
 
-export const getServerSideProps: GetServerSideProps<GamesProps> = async () => {
+export const getStaticProps: GetStaticProps<GamesProps> = async () => {
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query<QueryGames, QueryGamesVariables>({
+    query: QUERY_GAMES,
+    variables: { limit: 9 }
+  })
+
   return {
     props: {
-      games: gamesMock,
+      games: data.games.map((game) => ({
+        title: game.name,
+        img: `http://localhost:1337${game.cover?.url}`,
+        price: new Intl.NumberFormat('en-US', {
+          style: 'currency',
+          currency: 'USD'
+        }).format(game.price),
+        developer: game.developers[0].name
+      })),
       filterItems: filterItemsMock
     }
   }

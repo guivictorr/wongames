e4fee0a1345a3b3f2cff093b4bab56b2ff5d4108
guivictorr <55333929+guivictorr@users.noreply.github.com>
2022-02-04 20:36:17 -0300

    feat(queries): queryHome

--- src/graphql/queries/home.ts
+++ src/graphql/queries/home.ts
@@ -1,12 +1,66 @@
 import { gql } from '@apollo/client'
 import { BannerFragment } from 'graphql/fragments/banner'
+import { GameSliderFragment } from 'graphql/fragments/game'
+import { HighlightFragment } from 'graphql/fragments/highlight'
 
 export const QUERY_HOME = gql`
-  query QueryHome {
+  query QueryHome($today: String!, $limit: Int!) {
     banners {
       ...BannerFragment
     }
+    newGames: games(
+      where: { release_date_lte: $today }
+      sort: "release_date:desc"
+      limit: $limit
+    ) {
+      ...GameSliderFragment
+    }
+    upcomingGames: games(
+      where: { release_date_gt: $today }
+      sort: "release_date:asc"
+      limit: $limit
+    ) {
+      ...GameSliderFragment
+    }
+    freeGames: games(
+      where: { price: 0 }
+      sort: "release_date:asc"
+      limit: $limit
+    ) {
+      ...GameSliderFragment
+    }
+    sections: home {
+      newGames {
+        title
+        highlight {
+          ...HighlightFragment
+        }
+      }
+      upcomingGames {
+        title
+        highlight {
+          ...HighlightFragment
+        }
+      }
+      freeGames {
+        title
+        highlight {
+          ...HighlightFragment
+        }
+      }
+      popularGames {
+        title
+        highlight {
+          ...HighlightFragment
+        }
+        games {
+          ...GameSliderFragment
+        }
+      }
+    }
   }
 
+  ${GameSliderFragment}
+  ${HighlightFragment}
   ${BannerFragment}
 `

80d505538a6a722cc1ec23bdc4c8e4fb488a7f8b
guivictorr <55333929+guivictorr@users.noreply.github.com>
2022-02-04 21:24:32 -0300

    feat(home): connecting home with api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,11 @@
 import Home from 'templates/Home'
 import { HomeTemplateProps } from '../templates/Home'
 
-import cardSliderMock from 'components/GameCardSlider/mock'
-import highlightMock from 'components/Highlight/mock'
 import { GetStaticProps } from 'next'
 import { initializeApollo } from 'api/apollo'
-import { QueryHome } from 'graphql/generated/QueryHome'
+import { QueryHome, QueryHomeVariables } from 'graphql/generated/QueryHome'
 import { QUERY_HOME } from 'graphql/queries/home'
+import { bannerMapper, gameSliderMapper, highlightMapper } from 'utils/mapper'
 
 const apollo = initializeApollo()
 
@@ -15,33 +14,31 @@ export default function Index(props: HomeTemplateProps) {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const { data } = await apollo.query<QueryHome>({ query: QUERY_HOME })
+  const {
+    data: { banners, newGames, freeGames, upcomingGames, sections }
+  } = await apollo.query<QueryHome, QueryHomeVariables>({
+    query: QUERY_HOME,
+    variables: { limit: 8, today: new Date().toISOString() }
+  })
 
   return {
     revalidate: 60,
     props: {
-      banners: data.banners.map((banner) => ({
-        title: banner.title,
-        subtitle: banner.subtitle,
-        img: `http://localhost:1337${banner.image?.src}`,
-        ...(banner.button && {
-          buttonLabel: banner.button.label,
-          buttonLink: banner.button.link
-        }),
-        ...(banner.ribbon && {
-          ribbon: banner.ribbon.title,
-          ribbonColor: banner.ribbon.color,
-          ribbonSize: banner.ribbon.size
-        })
-      })),
-      newGames: cardSliderMock,
-      mostPopularHighlight: highlightMock,
-      mostPopularGames: cardSliderMock,
-      upcomingGames: cardSliderMock,
-      upcomingMoreGames: cardSliderMock,
-      upcomingHighlight: highlightMock,
-      freeGames: cardSliderMock,
-      freeHighlight: highlightMock
+      freeGamesTitle: sections?.freeGames && sections.freeGames.title,
+      mostPopularGamesTitle:
+        sections?.popularGames && sections.popularGames.title,
+      newGamesTitle: sections?.newGames && sections.newGames.title,
+      upcomingGamesTitle:
+        sections?.upcomingGames && sections.upcomingGames.title,
+      banners: bannerMapper(banners),
+      newGames: gameSliderMapper(newGames),
+      mostPopularHighlight: highlightMapper(sections?.popularGames?.highlight),
+      mostPopularGames: gameSliderMapper(sections?.popularGames?.games),
+      upcomingGames: gameSliderMapper(upcomingGames),
+      upcomingMoreGames: gameSliderMapper(upcomingGames),
+      upcomingHighlight: highlightMapper(sections?.upcomingGames?.highlight),
+      freeGames: gameSliderMapper(freeGames),
+      freeHighlight: highlightMapper(sections?.freeGames?.highlight)
     }
   }
 }

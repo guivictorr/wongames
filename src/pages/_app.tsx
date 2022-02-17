8d7346cd406806e0e2e78d498018584db2647fff
guivictorr <55333929+guivictorr@users.noreply.github.com>
2022-02-16 21:36:46 -0300

    feat(_app): adding CartProvider to the app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ApolloProvider } from '@apollo/client'
 import { useApollo } from 'api/apollo'
+import { CartProvider } from 'hooks/useCart'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
 import { ThemeProvider } from 'styled-components'
@@ -12,21 +13,23 @@ function App({ Component, pageProps }: AppProps) {
 
   return (
     <ApolloProvider client={client}>
-      <ThemeProvider theme={theme}>
-        <Head>
-          <title>React Avançado - Boilerplate</title>
-          <link rel="shortcut icon" href="/img/icon-512.png" />
-          <link rel="apple-touch-icon" href="/img/icon-512.png" />
-          <link rel="manifest" href="/manifest.json" />
-          <meta name="theme-color" content="#06092B" />
-          <meta
-            name="description"
-            content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
-          />
-        </Head>
-        <GlobalStyles />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <CartProvider>
+        <ThemeProvider theme={theme}>
+          <Head>
+            <title>React Avançado - Boilerplate</title>
+            <link rel="shortcut icon" href="/img/icon-512.png" />
+            <link rel="apple-touch-icon" href="/img/icon-512.png" />
+            <link rel="manifest" href="/manifest.json" />
+            <meta name="theme-color" content="#06092B" />
+            <meta
+              name="description"
+              content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+            />
+          </Head>
+          <GlobalStyles />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </CartProvider>
     </ApolloProvider>
   )
 }

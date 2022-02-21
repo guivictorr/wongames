17a3727b45e8ff681514e251642c86ebd06031bc
guivictorr <55333929+guivictorr@users.noreply.github.com>
2022-02-21 10:21:47 -0300

    feat(form): created form loading component

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -14,6 +14,13 @@ export const FormWrapper = styled.main`
   `}
 `
 
+export const FormLoading = styled.img.attrs(() => ({
+  src: '/img/dots.svg',
+  alt: 'Waiting...'
+}))`
+  width: 4rem;
+`
+
 export const FormLink = styled.div`
   ${({ theme }) => css`
     font-size: ${theme.font.sizes.small};

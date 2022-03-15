e6e066e32f2ea99b12a3ae5e9465935475ab1343
guivictorr <55333929+guivictorr@users.noreply.github.com>
2022-03-14 23:43:47 -0300

    feat(utils): adding resetValidate and forgotValidate functions

--- src/utils/validators/index.ts
+++ src/utils/validators/index.ts
@@ -44,3 +44,24 @@ export function signInValidate(values: SignInValues) {
   const validation = schema.validate(values, { abortEarly: false })
   return getFieldErrors(validation)
 }
+
+type ForgotValidateParams = Pick<UsersPermissionsRegisterInput, 'email'>
+export function forgotValidate(values: ForgotValidateParams) {
+  const { email } = fieldValidators
+
+  const schema = Joi.object({ email })
+  const validation = schema.validate(values, { abortEarly: false })
+  return getFieldErrors(validation)
+}
+
+type ResetValidateParams = {
+  password: string
+  confirm_password: string
+}
+export function resetValidate(values: ResetValidateParams) {
+  const { password, confirm_password } = fieldValidators
+
+  const schema = Joi.object({ password, confirm_password })
+  const validation = schema.validate(values, { abortEarly: false })
+  return getFieldErrors(validation)
+}
--- src/utils/validators/test.ts
+++ src/utils/validators/test.ts
@@ -1,4 +1,9 @@
-import { signInValidate, signUpValidate } from '.'
+import {
+  forgotValidate,
+  resetValidate,
+  signInValidate,
+  signUpValidate
+} from '.'
 
 describe('Validators', () => {
   describe('signInValidate()', () => {
@@ -64,6 +69,57 @@ describe('Validators', () => {
       }
 
       expect(signUpValidate(values)).toMatchInlineSnapshot(`
+        Object {
+          "confirm_password": "confirm password does not match with password",
+        }
+      `)
+    })
+  })
+
+  describe('forgotValidate()', () => {
+    it('should validate empty fields', () => {
+      const values = { email: '' }
+
+      expect(forgotValidate(values)).toMatchObject({
+        email: '"email" is not allowed to be empty'
+      })
+    })
+
+    it('should validate valid email', () => {
+      const values = { email: 'invalid-email' }
+
+      expect(forgotValidate(values)).toMatchObject({
+        email: '"email" must be a valid email'
+      })
+    })
+  })
+
+  describe('resetValidate()', () => {
+    it('should validate empty fields', () => {
+      const values = { password: '', confirm_password: '' }
+
+      expect(resetValidate(values)).toMatchObject({
+        password: '"password" is not allowed to be empty'
+      })
+    })
+
+    it('should validate confirm_password when empty', () => {
+      const values = { password: '123', confirm_password: '' }
+
+      expect(resetValidate(values)).toMatchInlineSnapshot(`
+        Object {
+          "confirm_password": "\\"confirm_password\\" is not allowed to be empty",
+        }
+      `)
+    })
+
+    it('should validate confirm_password', () => {
+      const values = {
+        password: '1234',
+        confirm_password: '12345'
+      }
+
+      expect(resetValidate(values)).toMatchInlineSnapshot(`
         Object {
           "confirm_password": "confirm password does not match with password",
         }

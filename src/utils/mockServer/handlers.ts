031115e0d3ae0c90943c548c9fba349050faa593
guivictorr <55333929+guivictorr@users.noreply.github.com>
2022-03-17 21:19:22 -0300

    test(forgot password): creating handler with msw for forgot password route

--- src/utils/mockServer/handlers.ts
+++ src/utils/mockServer/handlers.ts
@@ -1,1 +1,40 @@
-export const handlers = []
+import { rest } from 'msw'
+
+type LoginReqBody = {
+  email: string
+  password: string
+}
+
+export const handlers = [
+  rest.post<LoginReqBody>(
+    `${process.env.NEXT_PUBLIC_API_URL}/auth/forgot-password`,
+    (req, res, ctx) => {
+      const { email } = req.body
+
+      if (email === 'fail@gmail.com') {
+        return res(
+          ctx.status(400),
+          ctx.json({
+            error: 'Bad Request',
+            message: [
+              {
+                messages: [
+                  {
+                    message: 'This email does not exist '
+                  }
+                ]
+              }
+            ]
+          })
+        )
+      }
+
+      return res(
+        ctx.status(200),
+        ctx.json({
+          ok: true
+        })
+      )
+    }
+  )
+]

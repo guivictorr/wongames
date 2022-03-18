995b7f29b88d72b79137e38389dd77b877e35486
guivictorr <55333929+guivictorr@users.noreply.github.com>
2022-03-17 22:54:01 -0300

    test(reset password): adding tests to reset password form

--- src/components/FormResetPassword/test.tsx
+++ src/components/FormResetPassword/test.tsx
@@ -1,8 +1,16 @@
-import { render, screen } from 'utils/test-utils'
+import 'server.mock'
+import userEvent from '@testing-library/user-event'
+import { render, screen, waitFor } from 'utils/test-utils'
 import FormResetPassword from '.'
 
-jest.mock('next/router', () => ({
-  useRouter: jest.fn(() => ({ push: jest.fn() }))
+let query = {}
+const push = jest.fn()
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const routerMock = jest.spyOn(require('next/router'), 'useRouter')
+
+routerMock.mockImplementation(() => ({
+  query,
+  push
 }))
 
 describe('<FormResetPassword />', () => {
@@ -15,5 +23,44 @@ describe('<FormResetPassword />', () => {
     ).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should show error if passwords does not match', async () => {
+    render(<FormResetPassword />)
+
+    userEvent.type(screen.getByPlaceholderText('Password'), '123')
+    userEvent.type(screen.getByPlaceholderText(/confirm password/i), '321')
+
+    userEvent.click(screen.getByRole('button', { name: /redefine password/i }))
+
+    expect(
+      await screen.findByText(/confirm password does not match with password/i)
+    ).toBeInTheDocument()
+  })
+
+  it('should show error when code provided is wrong', async () => {
+    query = { code: 'wrong' }
+    render(<FormResetPassword />)
+
+    userEvent.type(screen.getByPlaceholderText('Password'), '123')
+    userEvent.type(screen.getByPlaceholderText(/confirm password/i), '123')
+
+    userEvent.click(screen.getByRole('button', { name: /redefine password/i }))
+
+    expect(await screen.findByText(/This code is invalid/i)).toBeInTheDocument()
+  })
+
+  it('should reset password and signin if is all valid', async () => {
+    query = { code: 'valid' }
+    render(<FormResetPassword />)
+
+    userEvent.type(screen.getByPlaceholderText('Password'), '123')
+    userEvent.type(screen.getByPlaceholderText(/confirm password/i), '123')
+
+    userEvent.click(screen.getByRole('button', { name: /redefine password/i }))
+
+    await waitFor(() => {
+      expect(push).toHaveBeenCalledWith('/sign-in')
+    })
   })
 })
--- src/utils/mockServer/handlers.ts
+++ src/utils/mockServer/handlers.ts
@@ -4,6 +4,12 @@ type LoginReqBody = {
   email: string
 }
 
+type ResetReqBody = {
+  code: string
+  password: string
+  confirm_password: string
+}
+
 export const handlers = [
   rest.post<LoginReqBody>(
     `${process.env.NEXT_PUBLIC_API_URL}/auth/forgot-password`,
@@ -32,6 +38,39 @@ export const handlers = [
         ctx.status(200),
         ctx.json({
           ok: true
+        })
+      )
+    }
+  ),
+  rest.post<ResetReqBody>(
+    `${process.env.NEXT_PUBLIC_API_URL}/auth/reset-password`,
+    (req, res, ctx) => {
+      const { code } = req.body
+
+      if (code === 'wrong') {
+        return res(
+          ctx.status(400),
+          ctx.json({
+            error: 'Bad Request',
+            message: [
+              {
+                messages: [
+                  {
+                    message: 'This code is invalid'
+                  }
+                ]
+              }
+            ]
+          })
+        )
+      }
+
+      return res(
+        ctx.status(200),
+        ctx.json({
+          user: {
+            email: 'valid@gmail.com'
+          }
         })
       )
     }

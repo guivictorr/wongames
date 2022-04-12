91a9cffb71fba96e4cb3c16f98baf6d9ca33cdb3
guivictorr <55333929+guivictorr@users.noreply.github.com>
2022-04-12 19:19:40 -0300

    feat(mappers): created a mapper to orders

--- src/utils/mappers/index.ts
+++ src/utils/mappers/index.ts
@@ -1,6 +1,7 @@
 import { HighlightFragment } from 'graphql/generated/HighlightFragment'
 import { QueryGames_games } from 'graphql/generated/QueryGames'
 import { QueryHome_banners } from 'graphql/generated/QueryHome'
+import { QueryOrders_orders } from 'graphql/generated/QueryOrders'
 import { formatNumber } from 'utils/format'
 
 export const bannerMapper = (banners: QueryHome_banners[]) => {
@@ -65,4 +66,37 @@ export const cartMapper = (games: QueryGames_games[] | null | undefined) => {
     }),
     title: game.name
   }))
+}
+
+export const ordersMapper = (orders: QueryOrders_orders[]) => {
+  return orders
+    ? orders.map((order) => {
+        return {
+          id: order.id,
+          paymentInfo: {
+            flag: order.card_brand,
+            img: order.card_brand ? `/img/cards/${order.card_brand}.png` : null,
+            number: order.card_last4
+              ? `**** **** **** ${order.card_last4}`
+              : 'Free Game',
+            purchaseDate: `Purchase made on ${new Intl.DateTimeFormat('en-US', {
+              day: 'numeric',
+              month: 'short',
+              year: 'numeric'
+            }).format(new Date(order.created_at))}`
+          },
+          games: order.games.map((game) => ({
+            id: game.id,
+            title: game.name,
+            downloadLink:
+              'https://wongames.com/game/download/yuYT56Tgh431LkjhNBgdf',
+            img: `http://localhost:1337${game.cover?.url}`,
+            price: formatNumber(game.price, {
+              style: 'currency',
+              currency: 'USD'
+            })
+          }))
+        }
+      })
+    : []
 }

5028a43d805469dd6f934184df88410327b520a1
guivictorr <55333929+guivictorr@users.noreply.github.com>
2022-04-13 19:46:30 -0300

    chore(next-auth): updating next-auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,12 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import NextAuth from 'next-auth'
+import NextAuth, { Session, User } from 'next-auth'
+import { JWT } from 'next-auth/jwt'
 import Providers from 'next-auth/providers'
-import { GenericObject } from 'next-auth/_utils'
+
+type AuthorizeProps = {
+  identifier: string
+  password: string
+}
 
 const options = {
   pages: {
@@ -11,7 +16,7 @@ const options = {
     Providers.Credentials({
       name: 'Sign-in',
       credentials: {},
-      async authorize(credentials) {
+      async authorize(credentials: AuthorizeProps) {
         const response = await fetch(
           `${process.env.NEXT_PUBLIC_API_URL}/auth/local`,
           {
@@ -31,16 +36,16 @@ const options = {
     })
   ],
   callbacks: {
-    session: async (session: GenericObject, user: GenericObject) => {
+    session: async (session: Session, user: User) => {
       session.jwt = user.jwt
       session.id = user.id
       return Promise.resolve(session)
     },
-    jwt: async (token: GenericObject, user: GenericObject) => {
+    jwt: async (token: JWT, user: User) => {
       if (user) {
         token.id = user.id
         token.email = user.email
-        token.name = user.username
+        token.name = user.username as string
         token.jwt = user.jwt
       }
 
